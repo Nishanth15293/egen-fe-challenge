2d4233091b827e52539561dd588d7cb8f3bbc8b3
Nishanth15293 <777.nishanth@gmail.com>
2017-01-29 18:01:42 -0500

    gulp js files ordering

--- dist/egen_cclogo.js
+++ dist/egen_cclogo.js
@@ -1,4 +1,10 @@
 (function(){
+	'use-strict';
+	angular.module('CreditCard', []);
+})();
+
+
+(function(){
 	angular.module('CreditCard').directive('cclogo', cclogo);
 
 	function cclogo(){
@@ -19,69 +25,7 @@
 						</div>'
 		}
 	}
-
-
-
-
-
 })();
-
-describe('cclogoController', function() {
-	'use strict';
-
-	var $controller;
-	var ctrl;
-
-	beforeEach(module('CreditCard'));
-
-	beforeEach(inject(function (_$controller_) {
-		$controller = _$controller_;
-	}));
-
-
-	describe('#detectCardType', function() {
-
-		it('detectCardType gives correctType', function() {
-			init();
-			var card = ctrl.detectCardType(ctrl.cardNumber);
-			expect(card).toEqual("Visa");
-			card = ctrl.detectCardType("5178058034818354");
-			expect(card).toEqual("Mastercard");
-		});
-	});
-
-
-	describe('#validCreditCard', function() {
-
-		it('validCreditCard gives card is valid or not', function() {
-			init();
-			var card = ctrl.validCreditCard(ctrl.cardNumber);
-			expect(card).toBe(true);
-			card = ctrl.validCreditCard("4147202249805994");
-			expect(ctrl.valid).toBe(false);
-		});
-	});
-
-	describe('#checkcard', function() {
-
-		it('checkcard gives card is valid or not', function() {
-			init();
-			var card = ctrl.checkcard(ctrl.cardNumber);
-			expect(card).toBe(true);
-
-			card = ctrl.checkcard("4147202249805994");
-			expect(ctrl.valid).toBe(false);
-
-		});
-	});
-
-	function init() {
-		ctrl = $controller('cclogoController');
-		ctrl.cardNumber = "4147202249805993";
-	}
-})
-
-
 
 
 (function() {
@@ -149,8 +93,4 @@ describe('cclogoController', function() {
         }
     }
 
-})();
-(function(){
-	'use-strict';
-	angular.module('CreditCard', []);
 })();
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,14 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
  
 gulp.task('jsbundle', function() {
-  return gulp.src('./js/*.js')
+
+	var injectJsFiles = [
+        './js/app.module.creditCard.js',
+        './js/app.ccLogo.js',
+        './js/app.ccLogoDirectiveController.js'
+    ];
+
+  return gulp.src(injectJsFiles)
     .pipe(concat('egen_cclogo.js'))
     .pipe(gulp.dest('./dist/'));
 });
